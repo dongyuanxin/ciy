01b31248198efaaabb3a4c5d153417a5061e1036
yuanxindong <yuanxin.me@gmail.com>
2020-02-24 17:06:12 +0800

    event: manage event with babel and derorator

---
+++ nodejs/manage-event.js
@@ -0,0 +1,56 @@
+// 管理大型项目中的事件机制，为了防止随意添加事件导致错乱
+// 使用一个白名单，每次操作前进行检查
+// script: npx babel manage-event.js --out-dir lib && node lib/manage-event.js
+
+const { EventEmitter } = require('events')
+
+// 相关操作前进行检查
+function check(target, name, descriptor) {
+    const method = descriptor.value
+
+    descriptor.value = function (event, ...args) {
+        if (!(event in this._eventList)) {
+            throw new Error(`事件名 ${event} 不合法`)
+        }
+        method.call(this, event, ...args)
+    }
+}
+
+class SafeEventEmitter extends EventEmitter {
+    constructor(eventList, ...params) {
+        super(...params)
+        this._eventList = eventList
+    }
+
+    @check
+    on(event, listener) {
+        return super.on(event, listener)
+    }
+
+    @check
+    emit(event, ...args) {
+        return super.emit(event, ...args)
+    }
+}
+
+///////////////
+
+// 事件名白名单
+const eventList = {
+    play: 'play',
+    pause: 'pause'
+}
+
+const player = new SafeEventEmitter(eventList)
+
+try {
+    player.on('play', () => {
+        console.log('play music')
+    })
+
+    player.on('kashfuahsodi', () => {
+        console.log('play music')
+    })
+} catch (error) {
+    console.log(error.message)
+}
