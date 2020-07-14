591fd2b14ba05f8611eab642ff8c484f988a84fb
yuanxindong <yuanxin.me@gmail.com>
2020-07-14 19:59:35 +0800

    add: avl, skip-list and offer NO.62

---
+++ algorithm/avl/index.js
@@ -0,0 +1,5 @@
+// 详解 AVL 树（基础篇） - Name1e5s的文章 - 知乎
+// https://zhuanlan.zhihu.com/p/34899732
+
+// 什么是平衡二叉树（AVL） - 程序员吴师兄的文章 - 知乎
+// https://zhuanlan.zhihu.com/p/56066942
---
+++ algorithm/skip-list/index.js
@@ -0,0 +1,3 @@
+// 跳跃表概念入门：https://juejin.im/post/5e05f50c6fb9a0161711ca44
+// 跳跃表go语言实现：https://zhuanlan.zhihu.com/p/72553601
+// 
